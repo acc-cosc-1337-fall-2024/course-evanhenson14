2035d2902f141189bc34c83a505bdb06c49ee2c6
evanhenson14 <evan.henson@g.austincc.edu>
2024-10-06 05:02:00 +0000

    hw 5

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,64 @@
 //add include statements
+#include"func.h"
+
 
 //add function code here
+
+
+double get_gc_content(const std::string& dna) 
+{
+    int gc_count = 0;
+
+    for (char content : dna) 
+    {
+       
+        if (content == 'G' || content == 'C') 
+        {
+            gc_count++;
+        }
+    }
+
+   
+    return static_cast<double>(gc_count) / dna.length();
+}
+
+
+
+std::string get_reverse_string(std::string dna) 
+{
+    int n = dna.length();
+    
+   
+    for (int i = 0; i < n / 2; ++i) 
+    {
+        std::swap(dna[i], dna[n - i - 1]);
+    }
+    return dna;
+}
+
+
+std::string get_dna_complement(std::string dna) 
+{
+
+    std::string reverse_dna = get_reverse_string(dna);
+
+    for (char &content : reverse_dna) 
+    {
+        switch (content) 
+        {
+            case 'A':
+                content = 'T';
+                break;
+            case 'T':
+                content = 'A';
+                break;
+            case 'C':
+                content = 'G';
+                break;
+            case 'G':
+                content = 'C';
+                break;
+        }
+    }
+    return reverse_dna;
+}
--- test/homework/05_functions/05_functions_tests.cpp
+++ test/homework/05_functions/05_functions_tests.cpp
@@ -1,7 +1,40 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include"func.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
+TEST_CASE("Verify Test Configuration", "verification") 
+{
 	REQUIRE(true == true);
+}
+
+
+
+TEST_CASE("Testing GC Content Calculation") 
+{
+    REQUIRE(get_gc_content("AGCTATAG") == Approx(0.375));
+    REQUIRE(get_gc_content("CGCTATAG") == Approx(0.50));
+    REQUIRE(get_gc_content("AATT") == Approx(0.0));
+    REQUIRE(get_gc_content("GGGG") == Approx(1.0));
+    REQUIRE(get_gc_content("") == Approx(0.0));
+}
+
+
+TEST_CASE("Testing DNA String Reversal") 
+{
+    REQUIRE(get_reverse_string("AGCTATAG") == "GATATCGA");
+    REQUIRE(get_reverse_string("CGCTATAG") == "GATATCGC");
+    REQUIRE(get_reverse_string("AAAACCCGGT") == "TGGCCAAAA");
+    REQUIRE(get_reverse_string("A") == "A");
+    REQUIRE(get_reverse_string("") == "");
+}
+
+
+TEST_CASE("Testing DNA Complement Calculation") 
+{
+    REQUIRE(get_dna_complement("AAAACCCGGT") == "ACCGGGTTTT");
+    REQUIRE(get_dna_complement("CCCGGAAAAT") == "ATTTTCCGGG");
+    REQUIRE(get_dna_complement("AGCT") == "TCGA");
+    REQUIRE(get_dna_complement("A") == "T");
+    REQUIRE(get_dna_complement("") == "");                     
 }
 
